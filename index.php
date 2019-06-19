db917dcc050e118f9881f72f2874ae860e900169
Lluis <lluiso.aloha@gmail.com>
2019-06-19 12:32:13 +0200

    init project

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ .  '/vendor/autoload.php';
+
+$articles = [
+    'premier article',
+    'deuxieme article',
+    'article cool',
+    'article marrant',
+    'article serieux'
+];
+
+var_dump($articles);
+
+dump($articles);
